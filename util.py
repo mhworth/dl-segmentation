4c5eaad3bd518c5093646df9335cda5463a417d5
Matt Hollingsworth <mr.hworth@gmail.com>
2016-06-02 22:52:26 +0000

    Made progress on classification notebook, utility functions, and updated environment

---
+++ util.py
@@ -0,0 +1,58 @@
+
+import sys, os, re
+import collections
+import numpy as np
+import scipy as sp
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+from cStringIO import StringIO
+
+WIDTH = 2048
+HEIGHT = 2048
+
+ClinicalInformation = collections.namedtuple('ClinicialInformation', ["filename"])
+
+# Some functions to load image
+def load_image_file(filename):
+    a = np.fromfile(filename, dtype=">i2")
+    raw_image = a.reshape((WIDTH, HEIGHT))
+    return raw_image
+
+def load_image_content(content):
+    a = np.fromstring(content, dtype=">i2")
+    raw_image = a.reshape((WIDTH, HEIGHT))
+    return raw_image
+
+def load_image_from_bucket(bucket, key_name):
+    key = bucket.get_key(key_name)
+    image = load_image_content(key.get_contents_as_string())
+    return image
+
+def resample_image(image16):
+    image16float = image16.astype(np.float32)
+    image8 = image16float*256/image16.max()
+    return image8.astype(np.uint8)
+
+
+def load_truth_data(bucket):
+    nodule_truth = bucket.get_key('Clinical_Information/nodule-truth-cleaned.csv')
+    non_nodule_truth = bucket.get_key('Clinical_Information/non-nodule-truth-cleaned.csv')
+    
+    nodule_truth_data, non_nodule_truth_data = nodule_truth.get_contents_as_string(), non_nodule_truth.get_contents_as_string()
+    sio = StringIO(nodule_truth_data)
+    sio.seek(0)
+    ntd = pd.read_csv(sio, sep=',')
+    
+    sio = StringIO(non_nodule_truth_data)
+    sio.seek(0)
+    nntd = pd.read_csv(sio, sep=',')
+    
+    truth = pd.concat([ntd, nntd])
+    truth = truth.set_index('filename')
+    return truth
+    
+
+# Load clinicial diagnoses
